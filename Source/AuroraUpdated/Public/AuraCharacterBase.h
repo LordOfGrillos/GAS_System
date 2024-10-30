5fed5eab72a700f63d787f542a4cec8579c7acc1
LordOfGrillos <139647784+LordOfGrillos@users.noreply.github.com>
2024-10-29 23:03:34 -0600

    Base Character Class

---
+++ Source/AuroraUpdated/Public/AuraCharacterBase.h
@@ -0,0 +1,29 @@
+// Copyright Zealix Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+UCLASS()
+class AURORAUPDATED_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAuraCharacterBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
